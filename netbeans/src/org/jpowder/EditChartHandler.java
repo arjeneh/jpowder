d9de421c45d01bbf4249b38c992c94cedd3e4668
kreecha <puphaiboon.kreecha@gmail.com>
2007-06-10 06:55:53 +0000

    Accept singleton EditChartFrameSingleton instead of EditChartFrame.java

--- netbeans/src/org/jpowder/EditChartHandler.java
+++ netbeans/src/org/jpowder/EditChartHandler.java
@@ -121,7 +121,8 @@ class EditChartDialog extends JFrame   {
                     //Thread safe by seperating it in case editing and modification.
                     java.awt.EventQueue.invokeLater(new Runnable() {
                         public void run() {
-                            EditChartFrame frame = new EditChartFrame(plot_copy);
+                            EditChartFrameSingleton obj = EditChartFrameSingleton.getSingletonObject(plot_copy);
+                            //EditChartFrameSingleton frame = new EditChartFrameSingleton(plot_copy);
                         }
                     });
                    
