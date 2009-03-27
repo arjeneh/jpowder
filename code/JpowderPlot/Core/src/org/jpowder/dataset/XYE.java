1be93396c9f971846e79b7d87ab6336253195837
Anders Markvardsen <anders.markvardsen@stfc.ac.uk>
2009-03-27 17:29:20 +0000

    Completed setting up new code structure splitting Kai code up into three sub-projects:
    Standalone - code specific to the swing GUI part of the standalone application
    Applet - code specific to the GUI part of the applet application (currently empty)
    Core - contains any code that will be used by both the Applet and Standalone projects (that is where most of Kai's code is copied to)
    
    Also the Standalone and Applet projects are setup to depend on the Core project.
    
    Thanks to Damian for more help on using netbeans, and have a nice weekend guys.

---
+++ code/JpowderPlot/Core/src/org/jpowder/dataset/XYE.java
@@ -0,0 +1,79 @@
+package org.jpowder.dataset;
+
+import java.util.Vector;
+import org.jpowder.util.VectorMiscUtil;
+
+/* 
+ * Creates a chart with (x,y, -y, +y).
+ */
+public class XYE extends DataSet {
+
+    private Vector e;
+    private Vector<Double> yLower;
+    private Vector<Double> yUpper;
+    private Vector data;
+
+    public XYE(Vector data, String fileName) {
+        super(data, fileName);
+
+        this.data = data;
+        this.e = VectorMiscUtil.getColumn(this.data, 2);
+
+        this.yLower = new Vector<Double>();
+        this.yUpper = new Vector<Double>();
+
+        Vector twoColumn = VectorMiscUtil.copyBeforeLastColumnsOf2DVector(this.data);
+        Vector outputOfMinusAdd = VectorMiscUtil.do_Minus_Addition_Y(twoColumn, this.e);
+        Vector result = VectorMiscUtil.getResultOfAddingTwoVectors(twoColumn, outputOfMinusAdd);
+
+        for (int rowIndex = 0; rowIndex < result.size(); rowIndex++) {
+            Vector row = (Vector) result.elementAt(rowIndex);
+            Double minusY = Double.parseDouble(row.elementAt(2).toString());
+            yLower.add(minusY);
+            Double plusY = Double.parseDouble(row.elementAt(3).toString());
+            yUpper.add(plusY);
+        }//end for 1
+
+        System.out.println(" In XYE.java yLower is " + yLower);
+        System.out.println(" In XYE.java y      is " + super.getY());
+        System.out.println(" In XYE.java yUpper is " + yUpper);
+    }
+
+    public DatasetPlotter createDatasetPlotter() {
+        return new ThreeColumnsPlotter(this);
+    }
+
+    public String description() {
+        return "XYE data contains " + this.data;
+    }
+
+    public Vector<Double> getE() {
+        return e;
+    }
+
+    public void setE(Vector<Double> e) {
+        this.e = e;
+    }
+
+    public Vector<Double> getYLower() {
+        return yLower;
+    }
+
+    public void setYLower(Vector<Double> yLower) {
+        this.yLower = yLower;
+    }
+
+    public Vector<Double> getYUpper() {
+        return yUpper;
+    }
+
+    public void setYUpper(Vector yUpper) {
+        this.yUpper = yUpper;
+    }
+    
+    @Override
+    public Vector getData() {
+        return this.data;
+    }
+}
+
