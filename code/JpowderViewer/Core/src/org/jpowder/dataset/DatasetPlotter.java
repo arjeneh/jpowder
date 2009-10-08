f901f1292a2e8ba004203e83ab6796a119bf0327
Anders Markvardsen <anders.markvardsen@stfc.ac.uk>
2009-10-08 13:54:20 +0000

    Renamed MultiFilesPlotter.java to FilesPlotter.java and changed code such that no need for ThreeColumnsPlotter.java and TwoColumnsPlotter.java. These these latter two classes where replicating code in MultiFilesPlotter.java, which is now renamed to FilesPlotter.java.

--- code/JpowderViewer/Core/src/org/jpowder/dataset/DatasetPlotter.java
+++ code/JpowderViewer/Core/src/org/jpowder/dataset/DatasetPlotter.java
@@ -19,26 +19,8 @@ public abstract class DatasetPlotter {
     public abstract org.jfree.chart.ChartPanel createPowderChart();
 
     public static DatasetPlotter createDatasetPlotter(Vector<DataSet> datasets){
-        if (datasets.size() > 1)
-          return new MultiFilesPlotter(datasets);
-        if (datasets.size() == 1)
-        {
-          // does the dataset contain 2 or 3 columns
-          DataSet lDataset = datasets.elementAt(0);
-
-          //if (lDataset instanceof XY)
-          if (lDataset.hasErrorbars)
-            return new ThreeColumnsPlotter(lDataset);
-          else
-            return new TwoColumnsPlotter(lDataset);
-          //if (lDataset instanceof XYE)
-
-          //else
-          //{
-          //  System.out.println("Not a recognised dataSet type");
-          //  return null;
-         // }
-        }
+        if (datasets.size() >= 1)
+          return new FilesPlotter(datasets);
         else
         {
           System.out.println("Can't create DatasetPlotter from no data");
@@ -47,15 +29,15 @@ public abstract class DatasetPlotter {
     }
 
   public static DatasetPlotter createDatasetPlotter(DataSet dataset) {
-    if (dataset instanceof DataSetNoErrors) {
-      return new TwoColumnsPlotter(dataset);
-    }
-    if (dataset instanceof DataSetWithErrors) {
-      return new ThreeColumnsPlotter(dataset);
-    } else {
-      System.out.println("Not a recognised dataset type");
-      return null;
-    }
+     if (dataset != null)
+     {
+       return new FilesPlotter(dataset);
+     }
+     else
+     {
+        System.out.println("Can't pass null in createDatasetPlotter");
+        return null;
+     }
   }
 }
 
