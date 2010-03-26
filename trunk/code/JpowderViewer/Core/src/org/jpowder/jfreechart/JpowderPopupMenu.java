2abc00c5baf30fac918c17629adbd88f4db052fd
m-arjeneh <m-arjeneh@b6ac7dbe-cf88-12de-82fe-cd48be0a5ca4>
2010-03-26 10:49:42 +0000

    Added some MenuItem to the PopUpMenu, removed the XY and XYE PopupMenu Class. I have created some Static methods in JpowderPopupMenu class so to get rid off the repeating codes in the JPowder class. Rename the the frame in the Menu Bar to Window. Fixed the problem with the CheckBox in DataVisibleInChart, now if a checkBox is unselected and window in focus changes the checkBox remain unchecked.

--- trunk/code/JpowderViewer/Core/src/org/jpowder/jfreechart/JpowderPopupMenu.java
+++ trunk/code/JpowderViewer/Core/src/org/jpowder/jfreechart/JpowderPopupMenu.java
@@ -4,62 +4,91 @@
  */
 package org.jpowder.jfreechart;
 
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.DefaultFontMapper;
+import com.lowagie.text.pdf.FontMapper;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfTemplate;
+import com.lowagie.text.pdf.PdfWriter;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+import java.awt.print.PageFormat;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFileChooser;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import org.jfree.chart.ChartColor;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.XYPlot;
 
 /**
- *
+ * This class created some new popupMenu and also amodifiies the default
+ * popupMenu of jfreechart.
  * @author Arjeneh
  */
 public class JpowderPopupMenu extends JPopupMenu implements ActionListener {
 
-    private JFreeChart chart;
+    private static JFreeChart chart;
     private JPopupMenu popupMenu;
     private ChartPanel chartPanel;
     private XYPlot plot;
     private double X = -1;
-    private JMenu zoomIn, zoomOut, autoRange;
+    public static final String PDF_CAMAND = "PDF";
+    public static final String JPOWDER_APPLET_CAMAND = "JPOWDER_APPLET";
+    public static final String PRINT_FOR_PUBLICATION_CAMAND = "PRINT_FOR_PUBLICATION";
+    private JMenu saveAs, printAs, zoomIn, zoomOut, autoRange;
     private JMenuItem menuItem;
-    //
 
     public JpowderPopupMenu(final ChartPanel chartPanel) {
 
         popupMenu = new JPopupMenu();
         this.chartPanel = chartPanel;
-        this.chart = this.chartPanel.getChart();
+        JpowderPopupMenu.chart = this.chartPanel.getChart();
         chartPanel.setPopupMenu(popupMenu);
         initComponents();
-//        this.plot = (XYPlot) this.chart.getPlot();
+        this.plot = (XYPlot) JpowderPopupMenu.chart.getPlot();
 
     }
 
+    /**
+     * all the popUpMenu Action.
+     * @param e
+     */
     public void actionPerformed(ActionEvent e) {
 
         String command = e.getActionCommand();
-        if (e.getActionCommand().contains("Properties")) {
+        if (command.equals(ChartPanel.PROPERTIES_COMMAND)) {
             chartPanel.doEditChartProperties();
 
-        } else if (e.getActionCommand().contains("Copy")) {
+        } else if (command.equals(ChartPanel.COPY_COMMAND)) {
             chartPanel.doCopy();
-        } //        else if (e.getActionCommand().contains("Save As...")) {
-        //            try {
-        //                chartPanel.doSaveAs();
-        //            } catch (IOException ex) {
-        //                Logger.getLogger(JpowderPopupMenu.class.getName()).log(Level.SEVERE, null, ex);
-        //            }
-        //        } else if (e.getActionCommand().contains("Print As...")) {
-        //            chartPanel.createChartPrintJob();
-        //        }
-        else if (command.equals(ChartPanel.ZOOM_IN_BOTH_COMMAND)) {
+        } else if (command.equals(ChartPanel.SAVE_COMMAND)) {
+            try {
+                chartPanel.doSaveAs();
+            } catch (IOException ex) {
+                Logger.getLogger(JpowderPopupMenu.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else if (command.equals(ChartPanel.PRINT_COMMAND)) {
+            chartPanel.createChartPrintJob();
+        } else if (command.equals(ChartPanel.ZOOM_IN_BOTH_COMMAND)) {
             chartPanel.zoomInBoth(X, X);
         } else if (command.equals(ChartPanel.ZOOM_IN_DOMAIN_COMMAND)) {
             chartPanel.zoomInDomain(X, X);
@@ -77,24 +106,56 @@ public class JpowderPopupMenu extends JPopupMenu implements ActionListener {
             chartPanel.restoreAutoDomainBounds();
         } else if (command.equals(ChartPanel.ZOOM_RESET_RANGE_COMMAND)) {
             chartPanel.restoreAutoRangeBounds();
+        } else if (command.equals(PRINT_FOR_PUBLICATION_CAMAND)) {
+            printForPublication();
+        } else if (command.equals(JPOWDER_APPLET_CAMAND)) {
+            saveAsJpowderApplet();
+        } else if (command.equals(PDF_CAMAND)) {
+            pDF();
         }
 
     }
 
+    /**
+     * All the popUpMenu created in the init mehtod.
+     */
     public void initComponents() {
 
 
         popupMenu.add(menuItem = new JMenuItem("Properties"));
+        menuItem.setActionCommand(ChartPanel.PROPERTIES_COMMAND);
+        menuItem.addActionListener(this);
+        popupMenu.addSeparator();
+
+
+        popupMenu.add(menuItem = new JMenuItem("Copy To ClipBoard"));
+        menuItem.setActionCommand(ChartPanel.COPY_COMMAND);
         menuItem.addActionListener(this);
 
-        popupMenu.add(menuItem = new JMenuItem("Copy"));
+
+
+        popupMenu.add(saveAs = new JMenu("Save As..."));
+        saveAs.add(menuItem = new JMenuItem("Jpowder Applet"));
+        menuItem.setActionCommand(JPOWDER_APPLET_CAMAND);
+        menuItem.addActionListener(this);
+        saveAs.add(menuItem = new JMenuItem("Image"));
+        menuItem.setActionCommand(ChartPanel.SAVE_COMMAND);
+        menuItem.addActionListener(this);
+        saveAs.add(menuItem = new JMenuItem("PDF"));
+        menuItem.setActionCommand(PDF_CAMAND);
         menuItem.addActionListener(this);
 
-//        popupMenu.add(menuItem = new JMenuItem("Save As..."));
-//        menuItem.addActionListener(this);
-//
-//        popupMenu.add(menuItem = new JMenuItem("Print As..."));
-//        menuItem.addActionListener(this);
+
+        popupMenu.addSeparator();
+        popupMenu.add(printAs = new JMenu("Print As..."));
+        printAs.add(menuItem = new JMenuItem("Print"));
+        menuItem.setActionCommand(ChartPanel.PRINT_COMMAND);
+        menuItem.addActionListener(this);
+        printAs.add(menuItem = new JMenuItem("Print For Publication"));
+        menuItem.setActionCommand(PRINT_FOR_PUBLICATION_CAMAND);
+        menuItem.addActionListener(this);
+
+
 
 
         popupMenu.addSeparator();
@@ -136,5 +197,134 @@ public class JpowderPopupMenu extends JPopupMenu implements ActionListener {
         autoRange.add(menuItem = new JMenuItem("Range Axis"));
         menuItem.setActionCommand(ChartPanel.ZOOM_RESET_RANGE_COMMAND);
         menuItem.addActionListener(this);
+    }
+
+    /**
+     * Printing the chart with plain and white background for publication porpuses.
+     */
+    public void printForPublication() {
+        plot.setBackgroundPaint(ChartColor.white);
+        plot.setOutlinePaint(ChartColor.white);
+        PrinterJob job = PrinterJob.getPrinterJob();
+        PageFormat pf = job.defaultPage();
+        PageFormat pf2 = job.pageDialog(pf);
+        if (pf2 != pf) {
+
+            job.setPrintable(chartPanel, pf2);
+            if (job.printDialog()) {
+                try {
+                    job.print();
+                } catch (PrinterException ex) {
+                    JOptionPane.showMessageDialog(this, ex);
+                }
+            }
+        }
+        plot.setBackgroundPaint(ChartColor.LIGHT_GRAY);
+        plot.setOutlinePaint(ChartColor.LIGHT_GRAY);
+    }
+
+    /**
+     * saving the file as serialazble so that can be saved and retrived into
+     * to Applet for web.
+     */
+    public static void saveAsJpowderApplet() {
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                ".Ser ", "Ser");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showSaveDialog(chooser);
+        File fileName = chooser.getSelectedFile();
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            try {
+                FileOutputStream buffer = new FileOutputStream(fileName);
+                final ObjectOutput out = new ObjectOutputStream(buffer);
+                out.writeObject(chart);
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "Invalid file!",
+                        "error", JOptionPane.ERROR_MESSAGE);
+            }
+        } else {
+        }
+    }
+
+    /**
+     * pdf file final saving.
+     */
+    public static void pDF() {
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                ".pdf ", "pdf");
+        chooser.setFileFilter(filter);
+
+        int returnVal = chooser.showSaveDialog(chooser);
+        File fileName = chooser.getSelectedFile();
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            try {
+                saveChartAsPDF(fileName, chart, 10000, 10000, new DefaultFontMapper());
+                System.out.println(fileName.getPath());
+                try //try statement
+                {
+                    Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + fileName.getPath());   //open the file chart.pdf
+
+                } catch (Exception e) //catch any exceptions here
+                {
+                    System.out.println("Error" + e);  //print the error
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * responsible for saving and writng the chart as pdf
+     * @param file
+     * @param chart
+     * @param width
+     * @param height
+     * @param mapper
+     * @throws IOException
+     */
+    public static void saveChartAsPDF(File file,
+            JFreeChart chart,
+            int width,
+            int height,
+            FontMapper mapper) throws IOException {
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+        writeChartAsPDF(out, chart, 1500, 2500, mapper);
+        out.close();
+    }
+
+    /**
+     *
+     * responsible for saving and writng the chart as pdf
+     * @param out
+     * @param chart
+     * @param width
+     * @param height
+     * @param mapper
+     * @throws IOException
+     */
+    public static void writeChartAsPDF(OutputStream out,
+            JFreeChart chart,
+            int width,
+            int height,
+            FontMapper mapper) throws IOException {
+        Rectangle pagesize = new Rectangle(width, height);
+        Document document = new Document(pagesize, 50, 50, 50, 50);
+        try {
+            PdfWriter writer = PdfWriter.getInstance(document, out);
+            document.open();
+            PdfContentByte cb = writer.getDirectContent();
+            PdfTemplate tp = cb.createTemplate(width, height);
+            Graphics2D g2 = tp.createGraphics(width, height, mapper);
+            Rectangle2D r2D = new Rectangle2D.Double(0, 0, width, height);
+            chart.draw(g2, r2D);
+            g2.dispose();
+            cb.addTemplate(tp, 0, 0);
+        } catch (DocumentException de) {
+            System.err.println(de.getMessage());
+        }
+        document.close();
     }
 }
--- trunk/code/JpowderViewer/Standalone/src/org/jpowder/JpowderInternalframe.java
+++ trunk/code/JpowderViewer/Standalone/src/org/jpowder/JpowderInternalframe.java
@@ -30,7 +30,6 @@ import org.jpowder.fileCabinet.PowderFileCabinet;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 
 /**
  *
