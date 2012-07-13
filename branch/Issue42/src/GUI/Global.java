1a1d4dc41f1634e80d761da28418f132162d8372
TristanJPerkins@gmail.com <TristanJPerkins@gmail.com@b6ac7dbe-cf88-12de-82fe-cd48be0a5ca4>
2012-07-13 11:56:58 +0000

    Code for Issue 42.

---
+++ branch/Issue42/src/GUI/Global.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+/**
+ *
+ * @author vcu98997
+ */
+public class Global {
+    public static int fileName;
+    
+}
